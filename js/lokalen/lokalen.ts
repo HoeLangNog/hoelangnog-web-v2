119c595205aaa80341e66dd0a4eaa252effb33fe
jaap <jaapelst@gmail.com>
2021-10-05 16:27:38 +0200

    fixed timezones

--- js/lokalen/lokalen.ts
+++ js/lokalen/lokalen.ts
@@ -18,20 +18,31 @@ window.onload = () => {
 
   let button = document.getElementById("submit-button");
 
-  button.onclick = () => {
-    let startTimeEl2 = document.getElementById("start-time");
-    let endTimeEl2 = document.getElementById("end-time");
+  button.onclick = onSearchClick;
+}
 
+function onSearchClick() {
+  let startTimeEl2 = document.getElementById("start-time");
+  let endTimeEl2 = document.getElementById("end-time");
+  let prefixEl = document.getElementById("prefix");
+  let foundEl = document.getElementById("found");
 
-    foundEl.innerHTML = "";
-    // @ts-ignore
-    let prefix = prefixEl.value;
-    // @ts-ignore
-    axiosa.default.get("https://api.hoelangnog.xyz/locations/available/" + prefix + "/?starttime=" + new Date(startTimeEl2.value).getTime() / 1000 + "&endtime=" + new Date(endTimeEl2.value).getTime() / 1000)
-      .then((response: AxiosResponse<Array<object>>) => {
-        response.data.forEach(a => {
-          foundEl.innerHTML += `<hln-location name="${a.code}">`;
-        })
+  foundEl.innerHTML = "";
+  // @ts-ignore
+  let prefix = prefixEl.value;
+
+  let startDate = new Date(startTimeEl2.value);
+  let endDate = new Date(endTimeEl2.value);
+  console.log(startDate.getTimezoneOffset());
+
+  startDate.setTime(startDate.getTime() - startDate.getTimezoneOffset() * 60000)
+  endDate.setTime(endDate.getTime() - endDate.getTimezoneOffset() * 60000)
+
+  // @ts-ignore
+  axiosa.default.get("https://api.hoelangnog.xyz/locations/available/" + prefix + "/?starttime=" + startDate.getTime() / 1000 + "&endtime=" + endDate.getTime() / 1000)
+    .then((response: AxiosResponse<Array<object>>) => {
+      response.data.forEach(a => {
+        foundEl.innerHTML += `<hln-location name="${a.code}">`;
       })
-  }
+    })
 }
