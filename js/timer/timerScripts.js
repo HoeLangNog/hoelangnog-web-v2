7ab3f81e77476686f9453602e2ff7a3d1414162a
Faab007NL <mail@fabiandingemans.nl>
2021-10-11 14:36:21 +0200

    Added groups dropdown

---
+++ js/timer/timerScripts.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function () {
+  let myStorage = window.localStorage || localStorage;
+  let calenderSelect = $("#calender-group-select");
+  calenderSelect.select2({
+    dropdownAutoWidth: true,
+    closeOnSelect: true
+  });
+  calenderSelect.on('change', function() {
+    let value = $('#calender-group-select').val();
+    myStorage.setItem('group', value);
+    loadSchedules(value);
+  });
+  $("select").on("select2:open", function(event) {
+    $('input.select2-search__field').attr('placeholder', 'Zoek naar een groep');
+  });
+});
