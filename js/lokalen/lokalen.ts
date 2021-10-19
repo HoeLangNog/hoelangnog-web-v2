89926e7af20e6e7c45148c1d4f301c3e9149829a
Faab007NL <mail@fabiandingemans.nl>
2021-10-19 15:37:32 +0200

    Added some extra styling to lokalen zoeker

--- js/lokalen/lokalen.ts
+++ js/lokalen/lokalen.ts
@@ -1,44 +1,55 @@
-import {formatTime} from "../timer/timerutils.js";
 // @ts-ignore
 import * as axiosa from "axios";
 // @ts-ignore
 import {AxiosResponse} from "axios";
 // @ts-ignore
 import DatePicker from 'tui-date-picker';
+// @ts-ignore
+import moment from 'moment';
 
 let searching = false;
-
-function onSelect() {
-
-}
 
 let button: HTMLElement;
 window.onload = () => {
   button = document.getElementById("submit-button");
 
-  let today = new Date();
-  let picker = DatePicker.createRangePicker({
-    timePicker: true,
-    format:'yyyy-MM-dd H:mm',
-    startpicker: {
-      date: today,
-      input: '#start-time',
-      container: '#start-time-container',
-      weekStartDay: 'mon'
-    },
-    endpicker: {
-      date: today,
-      input: '#end-time',
-      container: '#end-time-container',
-      weekStartDay: 'mon'
-    },
-    selectableRanges: [
-      [today, new Date(today.getFullYear(), today.getMonth() + 2, today.getDate())]
-    ]
+  $('#start-time').val(moment().format('MM/DD/YYYY HH:mm'));
+  $('#start-time').daterangepicker({
+    "timePicker": true,
+    "timePicker24Hour": true,
+    "singleDatePicker": true,
+    "autoUpdateInput": false,
+    "drops": "auto",
+    "minDate": moment().format('MM/DD/YYYY HH:mm'),
+    "maxDate": moment().add("2", "week").format('MM/DD/YYYY HH:mm')
+  }, function(start, end, label) {
+    $('#start-time').val(start.format('MM/DD/YYYY HH:mm'));
+    $('#end-time').val(start.add("1", "hour").format('MM/DD/YYYY HH:mm'));
+    $('#end-time').daterangepicker({
+      "timePicker": true,
+      "timePicker24Hour": true,
+      "singleDatePicker": true,
+      "autoUpdateInput": false,
+      "drops": "auto",
+      "minDate": start.format('MM/DD/YYYY HH:mm'),
+      "maxDate": moment().add("2", "week").format('MM/DD/YYYY HH:mm')
+    }, function(start, end, label) {
+      $('#end-time').val(start.format('MM/DD/YYYY HH:mm'));
+    });
   });
 
-  picker.setStartDate(today)
-  picker.setEndDate(today.setMinutes(today.getMinutes() + 30));
+  $('#end-time').val(moment().add("1", "hour").format('MM/DD/YYYY HH:mm'));
+  $('#end-time').daterangepicker({
+    "timePicker": true,
+    "timePicker24Hour": true,
+    "singleDatePicker": true,
+    "autoUpdateInput": false,
+    "drops": "auto",
+    "minDate": moment().format('MM/DD/YYYY HH:mm'),
+    "maxDate": moment().add("2", "week").format('MM/DD/YYYY HH:mm')
+  }, function(start, end, label) {
+    $('#end-time').val(start.format('MM/DD/YYYY HH:mm'));
+  });
 
   button.onclick = onSearchClick;
 }
@@ -48,19 +59,14 @@ function onSearchClick() {
     return;
   searching = true;
 
-  button.setAttribute("value", "");
-
-  button.classList.add("dot-flashing");
-  button.classList.remove("button");
-
-
-
   let startTimeEl2 = document.getElementById("start-time");
   let endTimeEl2 = document.getElementById("end-time");
   let prefixEl = document.getElementById("prefix");
   let foundEl = document.getElementById("found");
 
+  button.innerHTML = "<i class=\"fas fa-spinner fa-spin\"></i>";
   foundEl.innerHTML = "";
+  foundEl.classList.add("hidden");
   // @ts-ignore
   let prefix = prefixEl.value;
 // @ts-ignore
@@ -76,11 +82,9 @@ function onSearchClick() {
   axiosa.default.get("https://api.hoelangnog.xyz/locations/available/" + prefix + "/?starttime=" + startDate.getTime() / 1000 + "&endtime=" + endDate.getTime() / 1000)
     .then((response: AxiosResponse<Array<object>>) => {
       searching = false;
-      button.setAttribute("value", "Zoek");
 
-      button.classList.remove("dot-flashing");
-      button.classList.add("button");
-
+      foundEl.classList.remove("hidden");
+      button.innerHTML = "Zoek";
 
       response.data.forEach(a => {
         foundEl.innerHTML += `<hln-location name="${a.code}">`;
