68a8b7c3ce0e1931b8406eabcaffe6740fa7187c
Faab007NL <mail@fabiandingemans.nl>
2021-11-25 11:39:38 +0100

    Added calender dislaimer

---
+++ js/calender/disclaimerPopup.js
@@ -0,0 +1,22 @@
+import Swal from 'sweetalert2'
+let myStorage = window.localStorage || localStorage;
+
+let accepted = true
+if(myStorage.getItem("disclaimerAccepted") != null){
+  accepted = myStorage.getItem("disclaimerAccepted");
+}else{
+  myStorage.setItem("disclaimerAccepted", false);
+}
+
+if (accepted){
+  Swal.fire({
+    icon: 'warning',
+      title: 'Disclaimer!',
+      text: 'HoeLangNog is niet verantwoordelijk voor eventuele fouten in de calender!',
+      allowOutsideClick: false,
+      allowEscapeKey: false,
+      allowEnterKey: false
+  }).then((result) => {
+    myStorage.setItem("disclaimerAccepted", true);
+  });
+}
